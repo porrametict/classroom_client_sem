ba90c1794f8d5f2c43cae158ae25f0c7997b1e48
porrametict <60021414@up.ac.th>
2020-07-18 21:29:42 +0700

    add Primary btn

---
+++ src/components/share/PrimaryButton.vue
@@ -0,0 +1,14 @@
+<template>
+        <div>
+            <v-btn color="primary" @click="$emit('click')">
+                <slot>
+                    Primary
+                    </slot>
+            </v-btn>
+            </div>
+</template>
+<script>
+export default {
+    name : "PrimaryButton",
+}
+</script>
