dfb3f735f4b7927586fea80913a0f3e32632f442
porrametict <60021414@up.ac.th>
2020-07-18 23:21:02 +0700

    create login page

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,26 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors'
+
 
 Vue.use(Vuetify);
+const light_theme = {
+    sky: colors.blue.lighten3,
+}
+
+
+
+const dark_theme = {
+    sky: colors.blue.lighten3,
+}
+
+
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: light_theme,
+            dark: dark_theme,
+        },
+    }
 });
