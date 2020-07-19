e18f25a9470062d6f1002dc6ede36e716f749d41
DESKTOP-CFQ1PK9\Anony <ford19982541@gmail.com>
2020-07-19 13:01:28 +0700

    Dev.ford : Create and test cancel button.

---
+++ src/components/share/CancelButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <v-btn
+                color="grey lighten-1"
+                @click="$emit('click')"
+                rounded
+        >
+            <slot>
+                Cancel
+            </slot>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CancelButton"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/student/Create.vue
+++ src/views/student/Create.vue
@@ -2,14 +2,16 @@
     <div>
         Add student
         <save-button></save-button>
+        <cancel-button></cancel-button>
     </div>
 </template>
 
 <script>
     import SaveButton from "../../components/share/SaveButton";
+    import CancelButton from "../../components/share/CancelButton";
     export default {
         name: "createStudent",
-        components:{SaveButton}
+        components:{SaveButton,CancelButton}
     }
 </script>
 
