e22d89df815eaffebe3e48ce8d14afcd76fa5d3d
Suwantha <suwanthagg@gmail.com>
2020-07-28 18:31:59 +0700

    delelte course

--- src/views/course/Index.vue
+++ src/views/course/Index.vue
@@ -7,24 +7,24 @@
       </div>
       <div>
         <template>
-          <v-data-table
-          v-if="Courses"
-            :headers="headers"
-            :items="Courses"
-            class="elevation-1"
-            hide-default-footer
-          >
-            <template v-slot:item.manage="{ item }">
+          <v-data-table v-if="Courses" :headers="headers" :items="Courses" class="elevation-1">
+            <template v-slot:item.actions="{ item }">
               <div class="d-flex">
                 <v-btn icon>
                   <v-icon
                     color="orange lighten-2"
-                    @click="$router.push({name:'EditCourse',params : {id : item.id}})"
+                    @click="$router.push({name:'EditCourse'})"
                   >mdi-pencil</v-icon>
                 </v-btn>
-                <DeleteDialog Courses.id></DeleteDialog>
+                <DeleteDialog @change="deleteCourse($event,item)"></DeleteDialog>
               </div>
             </template>
+            <v-pagination
+              v-model="form_param.page"
+              :length="form_param.length"
+              circle
+              
+            ></v-pagination>
           </v-data-table>
         </template>
       </div>
@@ -36,24 +36,22 @@
 import PrimaryButton from "../../components/share/PrimaryButton";
 import Template from "../Template";
 import DeleteDialog from "../../components/share/DeleteDialog";
-import { delete } from 'vue/types/umd';
 
 export default {
   name: "CourseIndex",
-  components: { DeleteDialog, Template, PrimaryButton, },
+  components: { DeleteDialog, Template, PrimaryButton },
   data() {
     return {
       Courses: null,
-      form_param: {page :1},
-      
+      form_param: { search: null, page: 1, length: 0 },
+
       headers: [
         { text: "Course ID", value: "course_id", align: "start" },
         { text: "Full Name", value: "name" },
         { text: "Teacher Name", value: "teacher_name" },
         { text: "Status", value: "status" },
-        { text: "Manage", value: "manage", sortable: false },
+        { text: "Manage", value: "actions", sortable: false },
       ],
-      
     };
   },
   mounted() {
@@ -63,23 +61,17 @@ export default {
     async getCourse() {
       let Courses = await this.$store.dispatch(
         "course/getCourses",
-        this.form_param,
+        this.form_param
       );
-      this.Courses = Courses.results
-
+      this.Courses = Courses.results;
     },
-    async deleteCourse() {
-      if (confirm(DeleteDialog)){
-        let Courses = await this.$store.delete(
-        "course/deleteCourse", this.Courses.id)
+    async deleteCourse(params, item) {
+      if (params) {
+        let data = await this.$store.dispatch("course/deleteCourse", item.id);
         if (data != null) {
-          this.$router.go(-1)
-
+          await this.getCourse();
         }
-        
-
       }
-      
     },
   }
 };
