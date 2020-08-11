61e2ddda42c2e78852837dc30aa0d512091ccbb1
DESKTOP-CFQ1PK9\Anony <ford19982541@gmail.com>
2020-08-11 17:46:33 +0700

    Dev_ford : Add State course_id .

--- src/store/modules/student.js
+++ src/store/modules/student.js
@@ -1,9 +1,25 @@
 const student_api = 'api/v1/student/'
 export default {
     namespaced: true,
-    state: {},
-    mutations: {},
+    state: {
+        course_id:null
+    },
+    getters:{
+        course_id: (state) => {
+            return state.course_id
+        },
+    },
+    mutations: {
+        SET_COURSE_ID: (state, newValue) => {
+            state.course_id = newValue
+        },
+    },
     actions: {
+        setCourse_id: ({commit, state}, newValue) => {
+            commit('SET_COURSE_ID', newValue)
+            console.log(state.course_id,'inAction')
+            return state.course_id
+        },
         async createStudents(context, params) {
             return await axios.post(`${student_api}`, params)
                 .then((response) => {
@@ -16,8 +32,10 @@ export default {
                 })
         },
         async getStudents(context, params) {
+            console.log(params,'params')
             return await axios.get(`${student_api}`, params = {params})
                 .then((response) => {
+                    console.log(response.data,'response')
                     return response.data
                 })
                 .catch((error) => {
